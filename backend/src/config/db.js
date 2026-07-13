f3e8e50e1e8fa48579b56ae820ebb7f097a9ddb5
RohitAryan B <rohitaryan162006@gmail.com>
2026-07-13 17:03:38 +0530

    Updated db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,11 +3,10 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
-
         console.log("MongoDB Connected");
     } catch (error) {
-        console.log(error);
-        process.exit(1);
+        console.error("MongoDB connection error:", error.message);
+        setTimeout(connectDB, 5000);
     }
 };
 
